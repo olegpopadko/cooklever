154cf09b679f56e45ab54dc7081d1e8c8f407535
Oleg Popadko <oleg@popadko.com>
2015-10-31 17:21:35 +0200

    Customize FOSUser tempaltes

--- src/Cooklever/Bundle/AppBundle/Menu/Builder.php
+++ src/Cooklever/Bundle/AppBundle/Menu/Builder.php
@@ -8,20 +8,60 @@
 namespace Cooklever\Bundle\AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 
 class Builder extends ContainerAware
 {
+    /**
+     * @param FactoryInterface $factory
+     * @param array $options
+     * @return ItemInterface
+     */
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem(
             'CookLEVER',
             [
                 'route'  => 'homepage',
-                'extras' => ['menu_type' => 'topbar']
+                'extras' => ['menu_type' => 'topbar'],
             ]
         );
 
+        if (!$this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $this->buildAnonymous($menu);
+        } else {
+            $this->buildAuthenticated($menu);
+        }
+
         return $menu;
+    }
+
+    /**
+     * @param ItemInterface $menu
+     */
+    private function buildAuthenticated(ItemInterface $menu)
+    {
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        $menu->addChild('@' . $user->getUserName(), ['uri' => '#',]);
+        $menu->addChild(
+            $this->container->get('translator')->trans('layout.logout', [], 'FOSUserBundle'),
+            ['route' => 'fos_user_security_logout']
+        );
+    }
+
+    /**
+     * @param ItemInterface $menu
+     */
+    private function buildAnonymous(ItemInterface $menu)
+    {
+        $menu->addChild(
+            $this->container->get('translator')->trans('layout.register', [], 'FOSUserBundle'),
+            ['route' => 'fos_user_registration_register']
+        );
+        $menu->addChild(
+            $this->container->get('translator')->trans('layout.login', [], 'FOSUserBundle'),
+            ['route' => 'fos_user_security_login']
+        );
     }
 }
